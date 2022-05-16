4e63a06cb68ef020d7ada6b465c10dd9939443c7
novafirekt <95858066+novafirekt@users.noreply.github.com>
2022-05-17 01:06:25 +0530

    intial commit

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$connection = mysqli_connect('localhost','root','','tguide');
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $country = $_POST['country'];
+    $subject = $_POST['subject'];
+
+    $request = " insert into contact(name,	email,	number	,country,	subject) values('$name','$email','$number','$country' ,'$subject') ";
+
+    mysqli_query($connection , $request);
+    header('location:contact');
+    
+}else{
+    echo 'Something Went wrong ! ';
+}
+
+?>
